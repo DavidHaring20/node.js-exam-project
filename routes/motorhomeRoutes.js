c5f262c3d8cda409172324db0cd7bc4f15275c74
David <davidharingri@gmail.com>
2021-05-31 15:12:20 +0200

    Fixed

--- routes/motorhomeRoutes.js
+++ routes/motorhomeRoutes.js
@@ -34,39 +34,6 @@ router.get("/api/motorhomes", (req, res) => {
     res.send({ motorhomeArray });
 });
 
-exports.create=(req, res) => {
-    console.log("hit");
-    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-        if (error) {
-            throw new Error(error);
-        }
-    
-        const db = client.db(dbName);
-        const motorhomes = db.collection("motorhomes");
-    
-        motorhomes.insertOne(
-            { 
-                "brand" : req.body.brand,
-                "model" : req.body.model,
-                "type" : req.body.type,
-                "gasType" : req.body.gasType,
-                "numberOfSeats" : req.body.numberOfSeats,
-                "odometer" : req.body.odometer,
-                "yearOfManufacture" : req.body.yearOfManufacture,
-                "condition" : req.body.condition,
-                "additionalInfo" : req.body.additionalInfo 
-            }, (error, result) => {
-                if (error) {
-                    throw new Error(error);
-                } 
-    
-                console.log("Inserted count: " + result.insertedCount);
-    
-                client.close();
-            });
-    });
-}
-
 module.exports = {
     router
 };
