b78b8de1624a6203f23a751cf31089d7d456dc8e
David <davidharingri@gmail.com>
2021-06-01 14:47:50 +0200

    Fixed an error with not having css styles on homepage.html displayed.

--- app.js
+++ app.js
@@ -39,17 +39,22 @@ app.get('/motorhomes', (req, res) => {
     res.sendFile(__dirname + "/public/motorhomes/motorhomes.html");
 });
 
+app.get('/homepage', (req, res) => {
+    res.sendFile(__dirname + "/public/homePage/homePage.html");
+})
+
 app.get('/createnewmotorhome', (req, res) => {
     res.sendFile(__dirname + '/public/createnewmotorhome/createnewmotorhome.html');
 });
 
+console.log(__dirname);
 // Post HTTP requests
 app.post('/homePage', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     console.log(username + " " + password);
 
-    res.sendFile(__dirname + "/public/homePage/homePage.html");
+    res.redirect('/homepage');
 });
 
 app.post('/createnewmotorhome', (req, res) => {
